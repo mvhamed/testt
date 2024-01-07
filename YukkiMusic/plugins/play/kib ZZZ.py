99225b5ea7cd838802269e96d7106a33b435ceb0
mvhamed <145144709+mvhamed@users.noreply.github.com>
2024-01-07 17:04:11 +0200

    Update kib ZZZ.py

--- YukkiMusic/plugins/play/kib ZZZ.py
+++ YukkiMusic/plugins/play/kib ZZZ.py
@@ -13,12 +13,12 @@ REPLY_MESSAGE = "**صلي علي اشرف خلق الله 🥹✨**"
 @app.on_message(filters.regex("يـوتيوب. 📽"))
 def reply_to_HEY(Client, message):
     message.reply_photo(
-        photo=f"https://graph.org/file/6270c9db2449eab390115.jpg",
+        photo=f"https://telegra.ph/file/5ee75c8b81172a947c9eb.jpg",
         caption=f"""**يتم استخدام هذا الامر لعرض تحميل من اليوتيوب**\n**استخدم الامر بهذا الشكل** `تنزيل` ** او ** `يوتيوب` ** كمثل تنزيل سوره الرحمن اضغط علي الامر لنسخ والاستخدام**""",
         reply_markup=InlineKeyboardMarkup(
             [
             [
-                InlineKeyboardButton("˹  𝗭𝗲𝗹𝘇𝗮𝗹 ⁦. 𓌗", url=f"https://t.me/z_zzz8"),
+                InlineKeyboardButton("˹  𝙈𝙪𝙝𝙖𝙢𝙢𝙚𝙙 𝙆𝙝𝙖𝙡𝙞𝙙 ⁦. 𓌗", url=f"https://t.me/mvhmed"),
             ]
          ]
      )
